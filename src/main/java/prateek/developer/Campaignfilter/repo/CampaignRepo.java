c57071c159cc5b0a5c675c673108d1cc5b33d5e5
unthinkable-1319 <prateek.tripathi@unthinkable.co>
2022-06-28 11:04:13 +0530

    Change in Query

--- src/main/java/prateek/developer/Campaignfilter/repo/CampaignRepo.java
+++ src/main/java/prateek/developer/Campaignfilter/repo/CampaignRepo.java
@@ -18,9 +18,9 @@ public interface CampaignRepo extends JpaRepository<Campaign, String> {
     @Query("SELECT  c FROM Campaign c WHERE c.campaignId = ?1")
     public Campaign findByCampaignId(Integer campaignId);
 
-    @Query(value = "SELECT c FROM Campaign c WHERE c.campaignId=:campaignId or (:campaignName=' ' or :campaignName is null or c.campaignName=:campaignName ) or " +
-            "(:campaignStatus=' ' or :campaignStatus is null or c.campaignStatus IN :campaignStatus) or (:createdDate=' ' or :createdDate is null or c.createdDate=:createdDate) or " +
-    "(:startDate=' ' or :startDate is null or c.startDate=:startDate) or (:endDate=' ' or :endDate is null or c.endDate=:endDate)")
+    @Query(value = "SELECT c FROM Campaign c WHERE c.campaignId=:campaignId or (c.campaignName=:campaignName ) or " +
+            "(c.campaignStatus in :campaignStatus) or (c.createdDate=:createdDate) or " +
+    "(c.startDate=:startDate) or (c.endDate=:endDate)")
     public List<Campaign> getFilteredCampaigns(@Param("campaignId") Integer campaignId,
                                                @Param("campaignName") String campaignName,
                                                @Param("campaignStatus") List<CampaignStatus> campaignStatus,
